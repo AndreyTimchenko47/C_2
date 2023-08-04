0480bd088ab25266164084234e4a4578f9c72608
Andrey <Andrey@mail.ru>
2023-08-04 21:34:11 +0300

    Добавили несоклько задач из лекции № 2

---
+++ task_05/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result =arg1;
+    if(arg2> result) result =arg2;
+    if(arg3> result) result =arg3;
+    return result;
+}
+
+int a1 = 22;
+int a2 = 13;
+int a3 = 9;
+int a4 = 232;
+int a5 = -8;
+int a6 = 1;
+int a7 = 0;
+int a8 = -47;
+int a9 = 21;
+
+// int max1 =Max(a1, a2, a3);
+// int max2 =Max(a4, a5, a6);
+// int max3 =Max(a7, a8, a9);
+// int max =Max(max1, max2, max3);
+int max =Max(Max(a1, a2, a3), Max(a4, a5, a6), Max(a7, a8, a9));
+
+Console.WriteLine($"Максимальное число max = {max}");
+
---
+++ task_08/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] array = new int [10]; // создаем массив с 10 индексами
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position =-1; // не ноль так как -1 служит индикатором что искомого числа в массиве нет
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+        position= index;
+        break;
+        }
+        index++;
+    }
+    return position;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos =Indexof(array, 4);// показывает индекс числа 4 если оно имеется в массиве
+Console.WriteLine(pos);
